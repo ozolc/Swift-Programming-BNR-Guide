6a8146e23307433a63b8ba4f63279f2b5d50f905
Maksim Nosov <ozolc@yandex.ru>
2018-06-18 16:37:44 +0300

    Commit

---
+++ ch02/Variables.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stop lights."
+print(townDescription)
